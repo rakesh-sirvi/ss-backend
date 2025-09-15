2c27a4f589787e29e52f49d98883ac8397a5c925
Rakesh Sirvi <rakusirvi797@gmail.com>
2025-09-15 21:59:10 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import dotenv from "dotenv";
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const PORT = 8080;
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.get("/", (_request, response) => {
+  return response.send({ ok: true });
+});
 
 app.listen(PORT, () => {
   console.log(`✓ Server running on http://localhost:${PORT}`);
